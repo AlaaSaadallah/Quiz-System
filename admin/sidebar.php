e6af73f10d03bee99d80971af7085dbf6f4642f5
alaa <alaa.saadallah96@gmail.com>
2021-08-17 09:53:34 +0200

    admin sidebar links

---
+++ admin/sidebar.php
@@ -0,0 +1,43 @@
+<div class="sidebar-wrapper active">
+                <div class="sidebar-header">
+                    <div class="d-flex justify-content-between">
+                        <div class="logo">
+                            <a href="dashboardTemplate/dist/index.html"><img src="dashboardTemplate/dist/assets/images/logo/logo.png" alt="Logo" srcset=""></a>
+                        </div>
+                        <!-- <div class="toggler">
+                            <a href="#" class="sidebar-hide d-xl-none d-block"><i class="bi bi-x bi-middle"></i></a>
+                        </div> -->
+                    </div>
+                </div>
+                <div class="sidebar-menu">
+                    <ul class="menu">
+                        <li class="sidebar-title">Menu</li>
+                        <li class="sidebar-item active ">
+                            <a href="index.html" class='sidebar-link'>
+                                <i class="bi bi-grid-fill"></i>
+                                <span>Dashboard</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item  ">
+                            <a href="#" class='sidebar-link'>
+                                <i class="bi bi-stack"></i>
+                                <span>Users</span>
+                            </a>
+                          
+                        </li>
+                        <li class="sidebar-item  ">
+                            <a href="#" class='sidebar-link'>
+                                <i class="bi bi-collection-fill"></i>
+                                <span>Quizes</span>
+                            </a>
+                            
+                        </li>
+                        <li class="sidebar-item  ">
+                            <a href="#" class='sidebar-link'>
+                                <i class="bi bi-grid-1x2-fill"></i>
+                                <span>Requests</span>
+                            </a>
+                        
+                        </li>
+                       
+            </div>
