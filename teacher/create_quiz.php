08e3d002797a98f5d03540e62e253e2d61669f13
alaa <alaa.saadallah96@gmail.com>
2021-08-18 00:50:28 +0200

    create quiz for admin and teacher page design

--- teacher/create_quiz.php
+++ teacher/create_quiz.php
@@ -1,8 +1,46 @@
 <!-- form to write the question and the choices and detrmine the correct answer -->
+<link rel="stylesheet" href="dashboardTemplate/dist/assets/vendors/choices.js/choices.min.css" />
 <h1>Create Quiz</h1>
-<form></form>
+<form>
 <div class="card">
     <div class="card-body">
+    <div class="form-group row align-items-center">
+        <div class="col-md-6">
+                                                <div class="col-lg-2 col-3">
+                                                    <label class="col-form-label">Quiz Name :</label>
+                                                </div>
+                                                <div class="col-lg-10 col-9">
+                                                    <input type="text" id="first-name" class="form-control" name="fname"
+                                                        placeholder="Quiz Name">
+                                                </div>
+                                            </div>
+   
+                                            <div class="col-md-6">
+                                            <!-- <div class="form-group row align-items-center"> -->
+                                                <div class="col-lg-2 col-3">
+                                                    <label class="col-form-label">Grade:</label>
+                                                </div>
+                                                <div class="col-lg-10 col-9">
+                                                <div class="form-group">
+                                                    <select class="choices form-select multiple-remove"
+                                                        multiple="multiple">
+                                                        <optgroup label="Figures">
+                                                            <option value="romboid">Romboid</option>
+                                                            <option value="trapeze" selected>Trapeze</option>
+                                                            <option value="triangle">Triangle</option>
+                                                            <option value="polygon">Polygon</option>
+                                                        </optgroup>
+                                                        <optgroup label="Colors">
+                                                            <option value="red">Red</option>
+                                                            <option value="green">Green</option>
+                                                            <option value="blue" selected>Blue</option>
+                                                            <option value="purple">Purple</option>
+                                                        </optgroup>
+                                                    </select>
+                                                </div>
+                                                </div>
+                                            </div>
+    </div>
         <div class="form-group with-title mb-3">
             <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
             <label>Question</label>
@@ -52,8 +90,17 @@
 <a href="#" class="btn icon btn-primary" style="float: right;"><i data-feather="user"></i> <svg class="bi" width="1em" height="1em" fill="currentColor">
         <use xlink:href="dashboardTemplate/dist/assets/vendors/bootstrap-icons/bootstrap-icons.svg#plus-circle" />
     </svg> Add Question</a>
-</form>
 
+    <a href="#" class="btn icon btn-success" ><i data-feather="user"></i>Assign Quiz</a>
+</form>
+<script src="dashboardTemplate/dist/assets/vendors/choices.js/choices.min.js"></script>
+<script src="dashboardTemplate/dist/assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+    <script src="dashboardTemplate/dist/assets/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Include Choices JavaScript -->
+    <!-- <script src="assets/vendors/choices.js/choices.min.js"></script> -->
+
+    <script src="dashboardTemplate/dist/assets/js/main.js"></script>
 <!--                                         
 
                                             <script src="dashboardTemplate/dist/assets/vendors/quill/quill.min.js"></script>
