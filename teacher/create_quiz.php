0e924f6310427d1bbb16e00cb16ff2303e8eb4ea
alaa <alaa.saadallah96@gmail.com>
2021-08-18 00:33:25 +0200

    create quiz for admin and teacher page design

---
+++ teacher/create_quiz.php
@@ -0,0 +1,67 @@
+<!-- form to write the question and the choices and detrmine the correct answer -->
+<h1>Create Quiz</h1>
+<form></form>
+<div class="card">
+    <div class="card-body">
+        <div class="form-group with-title mb-3">
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            <label>Question</label>
+        </div>
+
+        <div class="input-group">
+            <div class="input-group-text">
+                <div class="form-check form-check-success">
+
+                    <input class="form-check-input" name="Success" id="Success" type="radio" value="" aria-label="Radio button for following text input">
+                </div>
+            </div>
+            <input type="text" class="form-control" aria-label="Text input with radio button">
+        </div>
+        <br>
+        <div class="input-group">
+            <div class="input-group-text">
+                <div class="form-check form-check-success">
+
+                    <input class="form-check-input" name="Success" id="Success" type="radio" value="" aria-label="Radio button for following text input">
+                </div>
+            </div>
+            <input type="text" class="form-control" aria-label="Text input with radio button">
+        </div>
+        <br>
+        <div class="input-group">
+            <div class="input-group-text">
+                <div class="form-check form-check-success">
+
+                    <input class="form-check-input" name="Success" id="Success" type="radio" value="" aria-label="Radio button for following text input">
+                </div>
+            </div>
+            <input type="text" class="form-control" aria-label="Text input with radio button">
+        </div>
+        <br>
+        <div class="input-group">
+            <div class="input-group-text">
+                <div class="form-check form-check-success">
+
+                    <input class="form-check-input" name="Success" id="Success" type="radio" value="" aria-label="Radio button for following text input">
+                </div>
+            </div>
+            <input type="text" class="form-control" aria-label="Text input with radio button">
+        </div>
+    </div>
+</div>
+<a href="#" class="btn icon btn-primary" style="float: right;"><i data-feather="user"></i> <svg class="bi" width="1em" height="1em" fill="currentColor">
+        <use xlink:href="dashboardTemplate/dist/assets/vendors/bootstrap-icons/bootstrap-icons.svg#plus-circle" />
+    </svg> Add Question</a>
+</form>
+
+<!--                                         
+
+                                            <script src="dashboardTemplate/dist/assets/vendors/quill/quill.min.js"></script>
+    <script src="dashboardTemplate/dist/assets/js/pages/form-editor.js"></script>
+    <script src="dashboardTemplate/dist/assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+    <script src="dashboardTemplate/dist/assets/js/bootstrap.bundle.min.js"></script>
+
+    <script src="dashboardTemplate/dist/assets/vendors/apexcharts/apexcharts.js"></script>
+    <script src="dashboardTemplate/dist/assets/js/pages/dashboard.js"></script>
+
+    <script src="dashboardTemplate/dist/assets/js/main.js"></script> -->
