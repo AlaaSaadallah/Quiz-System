ef88f39080c6717be53928022ff02e37d273dfdd
alaa <alaa.saadallah96@gmail.com>
2021-08-17 09:19:01 +0200

    view all users in a list

---
+++ admin/users.php
@@ -0,0 +1,83 @@
+<h1>users page</h1>
+
+<div class="card">
+    <div class="card-header">
+        Users Data
+        <span style="float: right;">
+            <a href="#" class="btn-sm icon icon-left btn-success"><i data-feather="alert-circle" style="float: right;"></i>
+                Add User</a>
+        </span>
+    </div>
+    <div class="card-body">
+        <table class="table table-striped" id="table1">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Role</th>
+
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <div class="avatar me-3">
+                            <img src="dashboardTemplate/dist/assets/images/faces/1.jpg" alt="" srcset="">
+                        </div>Graiden
+                    </td>
+                    <td>teacher</td>
+
+                    <td colspan="3">
+                        <a href="/quizSystem/profile.php" class="btn-sm icon icon-left btn-primary" style="margin-right: 2%;"><i data-feather="edit"></i>
+                            View</a>
+
+                        <a href="/quizSystem/admin/user_edit.php" class="btn-sm icon icon-left btn-warning" style="margin-right: 2%;"><i data-feather="alert-triangle"></i>
+                            Edit</a>
+
+                        <a href="#" class="btn-sm icon icon-left btn-danger" style="margin-right: 2%;"><i data-feather="alert-circle"></i>
+                            Delete</a>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <div class="avatar me-3">
+                            <img src="dashboardTemplate/dist/assets/images/faces/1.jpg" alt="" srcset="">
+                        </div>Graiden
+                    </td>
+                    <td>teacher</td>
+
+                    <td colspan="3">
+                        <a href="#" class="btn-sm icon icon-left btn-primary" style="margin-right: 2%;"><i data-feather="edit"></i>
+                            View</a>
+
+                        <a href="#" class="btn-sm icon icon-left btn-warning" style="margin-right: 2%;"><i data-feather="alert-triangle"></i>
+                            Edit</a>
+
+                        <a href="#" class="btn-sm icon icon-left btn-danger" style="margin-right: 2%;"><i data-feather="alert-circle"></i>
+                            Delete</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div class="avatar me-3">
+                            <img src="dashboardTemplate/dist/assets/images/faces/1.jpg" alt="" srcset="">
+                        </div>Graiden
+                    </td>
+                    <td>teacher</td>
+
+                    <td colspan="3">
+                        <a href="#" class="btn-sm icon icon-left btn-primary" style="margin-right: 2%;"><i data-feather="edit"></i>
+                            View</a>
+
+                        <a href="#" class="btn-sm icon icon-left btn-warning" style="margin-right: 2%;"><i data-feather="alert-triangle"></i>
+                            Edit</a>
+
+                        <a href="#" class="btn-sm icon icon-left btn-danger" style="margin-right: 2%;"><i data-feather="alert-circle"></i>
+                            Delete</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
