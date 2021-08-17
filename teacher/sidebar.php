ced4c83dc5ced30a7aa71934077d4cecab37e3c7
alaa <alaa.saadallah96@gmail.com>
2021-08-17 03:46:39 +0200

    teacher module

---
+++ teacher/sidebar.php
@@ -0,0 +1,48 @@
+
+<div class="sidebar-wrapper active">
+                <div class="sidebar-header">
+                    <div class="d-flex justify-content-between">
+                        <div class="logo">
+                            <a href="dashboardTemplate/dist/index.html"><img src="dashboardTemplate/dist/assets/images/logo/logo.png" alt="Logo" srcset=""></a>
+                        </div>
+                        <!-- <div class="toggler">
+                            <a href="#" class="sidebar-hide d-xl-none d-block"><i class="bi bi-x bi-middle"></i></a>
+                        </div> -->
+                    </div>
+                </div>
+                <div class="sidebar-menu">
+                    <ul class="menu">
+                        <li class="sidebar-title">Menu</li>
+
+                        <li class="sidebar-item active ">
+                            <a href="index.php" class='sidebar-link'>
+                                <i class="bi bi-grid-fill"></i>
+                                <span>Home</span>
+                            </a>
+                        </li>
+
+                        <li class="sidebar-item  ">
+                            <a href="#" class='sidebar-link'>
+                                <i class="bi bi-stack"></i>
+                                <span>Create Quiz</span>
+                            </a>
+                        
+                        </li>
+
+                        <li class="sidebar-item  ">
+                            <a href="#" class='sidebar-link'>
+                                <i class="bi bi-collection-fill"></i>
+                                <span>Edit Quiz</span>
+                            </a>
+                         
+                        </li>
+
+                        <li class="sidebar-item  ">
+                            <a href="#" class='sidebar-link'>
+                                <i class="bi bi-grid-1x2-fill"></i>
+                                <span>Quiz Result</span>
+                            </a>
+                        
+                        </li>
+
+            </div>
