2a3950f74bb40c327685274807f15b10e6edaca0
alaa <alaa.saadallah96@gmail.com>
2021-08-18 02:21:07 +0200

    view past quizes page design

---
+++ student/taken_quiz.php
@@ -0,0 +1,50 @@
+<!-- past quizes with hyperlink to student answer -->
+<!-- view model answer with highlight student answer -->
+
+<h1>Taken Quizes</h1>
+<div class="col-md-6 mb-4">
+                                                
+                                                <div class="input-group mb-3">
+                                                    <label class="input-group-text"
+                                                        for="inputGroupSelect01">Subject</label>
+                                                    <select class="form-select" id="inputGroupSelect01">
+                                                        <option selected>Choose...</option>
+                                                        <option value="1">One</option>
+                                                        <option value="2">Two</option>
+                                                        <option value="3">Three</option>
+                                                    </select>
+                                               
+                                                  
+                                                   
+                                                </div>
+                                            </div>
+
+                                            <div class="card">
+
+    <div class="card-body">
+        <table class="table table-striped" id="table1">
+            <thead>
+                <tr>
+                    <th>Quiz Title</th>
+                    <th>Score</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Quiz 1</td>
+                    <td>10/10</td>
+                    
+                    <td >
+                        <a href="/quizSystem/profile.php" class="btn-sm icon icon-left btn-primary" style="margin-right: 2%;"><i data-feather="edit"></i>
+                            View Answers</a>
+
+                        
+                    </td>
+                </tr>
+
+                
+            </tbody>
+        </table>
+    </div>
+</div>
