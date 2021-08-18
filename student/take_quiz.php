1438336a6cf5fc033c100f8a79ccf9121b086188
alaa <alaa.saadallah96@gmail.com>
2021-08-18 02:17:08 +0200

    view available quizes page design

---
+++ student/take_quiz.php
@@ -0,0 +1,47 @@
+<!-- list all available quizes -->
+<!-- list students answer the quiz with hyperlink to their answers -->
+<h1>Available Quizes</h1>
+<div class="col-md-6 mb-4">
+                                                
+                                                <div class="input-group mb-3">
+                                                    <label class="input-group-text"
+                                                        for="inputGroupSelect01">Subject</label>
+                                                    <select class="form-select" id="inputGroupSelect01">
+                                                        <option selected>Choose...</option>
+                                                        <option value="1">One</option>
+                                                        <option value="2">Two</option>
+                                                        <option value="3">Three</option>
+                                                    </select>
+                                               
+                                                       </div>
+                                            </div>
+
+                                            <div class="card">
+
+    <div class="card-body">
+        <table class="table table-striped" id="table1">
+            <thead>
+                <tr>
+                    <th>Quiz Title</th>
+                   
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                  
+                    <td>Quiz 1</td>
+                    
+                    <td >
+                        <a href="/quizSystem/profile.php" class="btn-sm icon icon-left btn-primary" style="margin-right: 2%;"><i data-feather="edit"></i>
+                         Answer This Quiz</a>
+
+                        
+                    </td>
+                </tr>
+
+                
+            </tbody>
+        </table>
+    </div>
+</div>
