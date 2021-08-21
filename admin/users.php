02f335b9b0e215a7aebf1b07ce9a45f330292a1e
alaa <alaa.saadallah96@gmail.com>
2021-08-21 23:28:23 +0200

    list all users from database

--- admin/users.php
+++ admin/users.php
@@ -1,10 +1,16 @@
+<?php
+include "./queries.php";
+// include "./connection.php"
+?>
+
+
 <h1>users page</h1>
 
 <div class="card">
     <div class="card-header">
         Users Data
         <span style="float: right;">
-            <a href="#" class="btn-sm icon icon-left btn-success"><i data-feather="alert-circle" style="float: right;"></i>
+            <a href="<?php echo '?title=admin/add_user'?>" class="btn-sm icon icon-left btn-success"><i data-feather="alert-circle" style="float: right;"></i>
                 Add User</a>
         </span>
     </div>
@@ -19,27 +25,39 @@
                 </tr>
             </thead>
             <tbody>
+                <?php
+                    
+                       while(($x=mysqli_fetch_array($TeacherData)) || ($x= mysqli_fetch_array($StudentData)) || ($x=mysqli_fetch_array($AdminData)))
+                       { 
+                    ?> 
                 <tr>
                     <td>
                         <div class="avatar me-3">
                             <img src="dashboardTemplate/dist/assets/images/faces/1.jpg" alt="" srcset="">
-                        </div>Graiden
+                        </div><?php echo $x['FName'] ." " .$x['LName'] ?>
+                        <!-- <input type="text" value="<?php //echo $x=['Email'] ?>" > -->
                     </td>
-                    <td>teacher</td>
+                    <td><?php echo $x['role']. $x['UserID'] ?></td>
 
                     <td colspan="3">
-                        <a href="/quizSystem/profile.php" class="btn-sm icon icon-left btn-primary" style="margin-right: 2%;"><i data-feather="edit"></i>
+                        <a href="<?php echo '?email='. $x['Email'].'/title=profile'?>"class="btn-sm icon icon-left btn-primary" style="margin-right: 2%;"><i data-feather="edit"></i>
                             View</a>
-
-                        <a href="/quizSystem/admin/user_edit.php" class="btn-sm icon icon-left btn-warning" style="margin-right: 2%;"><i data-feather="alert-triangle"></i>
+                            
+                        <a href="<?php echo '?email='. $x['Email'].'&'.'title=admin/user_edit';?> "class="btn-sm icon icon-left btn-warning" style="margin-right: 2%;"><i data-feather="alert-triangle"></i>
                             Edit</a>
 
-                        <a href="#" class="btn-sm icon icon-left btn-danger" style="margin-right: 2%;"><i data-feather="alert-circle"></i>
+                        <a href="<?php echo '?email='. $x['Email'].'&'.'title=admin/delete_user';?> " class="btn-sm icon icon-left btn-danger" style="margin-right: 2%;"><i data-feather="alert-circle"></i>
                             Delete</a>
                     </td>
                 </tr>
+                <?php
+               
+            //    $row++;
+                    }
+                    
+                ?>
 
-                <tr>
+                <!-- <tr>
                     <td>
                         <div class="avatar me-3">
                             <img src="dashboardTemplate/dist/assets/images/faces/1.jpg" alt="" srcset="">
@@ -57,9 +75,9 @@
                         <a href="#" class="btn-sm icon icon-left btn-danger" style="margin-right: 2%;"><i data-feather="alert-circle"></i>
                             Delete</a>
                     </td>
-                </tr>
-                <tr>
-                    <td>
+                </tr> -->
+                <!-- <tr> -->
+                    <!-- <td>
                         <div class="avatar me-3">
                             <img src="dashboardTemplate/dist/assets/images/faces/1.jpg" alt="" srcset="">
                         </div>Graiden
@@ -76,7 +94,7 @@
                         <a href="#" class="btn-sm icon icon-left btn-danger" style="margin-right: 2%;"><i data-feather="alert-circle"></i>
                             Delete</a>
                     </td>
-                </tr>
+                </tr> -->
             </tbody>
         </table>
     </div>
